bfacb347c36902a0730f51c0aa13e669adf56f57
Petko Pedev <petko.pedev@gmail.com>
2021-10-17 18:40:55 +0300

    Uploading rest of the homework.

--- JSAdvanced/04-DOMIntroductionLab/02.Collect List Items/CollectListItems.js
+++ JSAdvanced/04-DOMIntroductionLab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,12 @@
 function extractText() {
-    // TODO
+    let itemNodes = document.querySelectorAll('ul#items li');
+    let textArea = document.querySelector('#result');
+
+    for (let node of itemNodes) {
+        textArea.value += node.textContent + '\n';
+    }
+
+    // const liElements = [...document.getElementsByTagName('li')];
+    // const elementText = liElements.map(e => e.textContent);
+    // document.getElementById('result').value = elementText.join('\n');
 }
--- JSAdvanced/04-DOMIntroductionLab/06.Sum Table/SumTable.js
+++ JSAdvanced/04-DOMIntroductionLab/06.Sum Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
-
+    let rows = document.querySelectorAll('table tr');
+    let total = 0;
+    for (let i = 1; i < rows.length - 1; i++) {
+        let cols = rows[i].children;
+        let cost = cols[cols.length - 1].textContent;
+        total += Number(cost);
+    }
+    document.getElementById('sum').textContent = total;
 }
