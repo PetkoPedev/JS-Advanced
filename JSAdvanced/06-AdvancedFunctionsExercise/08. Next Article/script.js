b55db3b1251f4de46bd73c5fa3c5ff6845d9169d
Petko Pedev <petko.pedev@gmail.com>
2021-10-26 13:39:06 +0300

    Adding Next Article to repo.

--- JSAdvanced/06-AdvancedFunctionsExercise/08. Next Article/script.js
+++ JSAdvanced/06-AdvancedFunctionsExercise/08. Next Article/script.js
@@ -1,3 +1,11 @@
 function getArticleGenerator(articles) {
-    // TODO
+    // TODO: redesign
+    let container = $('div#content');
+
+    return function () {
+        if (articles.length > 0) {
+            container.append($('<article>')
+                .text(articles.shift()));
+        }
+    }
 }
