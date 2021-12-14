938a21d53a26e86840fb269b4e990bafcf3f9e1b
Petko Pedev <petko.pedev@gmail.com>
2021-12-14 12:26:47 +0200

    Clientside Rendering exercise todo: fix.

---
+++ JSApplications/06-ClientSideRenderingExercise/02.HTTP-Status-Cats/app.js
@@ -0,0 +1,21 @@
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+import { cats } from "./catSeeder.js";
+import { allCatsTemplate } from "./templates/catTemplates.js";
+
+function toggleStatusCodeButton(e) {
+    e.preventDefault();
+    let button = e.target;
+    button.textContent = button.textContent === 'Show status code'
+        ? 'Hide status code'
+        : 'Show status code';
+    let infoDiv = button.closest('.info');
+    let statusDiv = infoDiv.querySelector('.status');
+    if (statusDiv.classList.contains('hidden')) {
+        statusDiv.classList.remove('hidden');   
+    } else {
+        statusDiv.classList.add('hidden');
+    }
+}
+
+let allCatsSection = document.getElementById('allCats');
+render(allCatsTemplate(cats, toggleStatusCodeButton), allCatsSection);
